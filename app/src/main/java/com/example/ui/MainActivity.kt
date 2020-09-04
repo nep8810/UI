94a34179b4f5d4e0729b86fdddaf4235e7c92983
nep8810 <>
2020-09-04 09:27:25 +0900

    Button Log

---
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            Log.d("UI_PARTS", "ボタンをタップしました")
+        }
+
+    }
+}
